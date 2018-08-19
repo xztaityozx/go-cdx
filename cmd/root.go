f7ce0d1b40aad1fd9f234b143b984500810d5667
xztaityozx <taityonohibi@gmail.com>
2018-08-19 22:23:52 +0900

    fixed #16

--- cmd/root.go
+++ cmd/root.go
@@ -70,7 +70,7 @@ var rootCmd = &cobra.Command{
 		dst := GetDestination(args)
 		if addBookmark {
 			AppendRecord(dst, config.BookMarkFile)
-			fmt.Println("[cdx] Bookmark <-", dst)
+			fmt.Println("echo [cdx] Bookmark <-", dst)
 			os.Exit(0)
 		}
 
