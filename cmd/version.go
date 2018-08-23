267b4b62ea30d45889b44d6d8e4d18c6dc24cd77
xztaityozx <taityonohibi@gmail.com>
2018-08-23 14:40:30 +0900

    V1.1 (#19)
    * add action
    
    * #18

--- cmd/version.go
+++ cmd/version.go
@@ -8,9 +8,9 @@ import (
 var version Version = Version{
 	Major:  1,
 	Minor:  1,
-	Build:  18,
+	Build:  19,
 	Status: "Beta",
-	Date:   "2018/08/19",
+	Date:   "2018/08/23",
 }
 
 type Version struct {
