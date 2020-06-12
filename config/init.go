5f43e8380885b73356c47ec0cfa23f80d7785ca4
xztaityozx <taityonohibi@gmail.com>
2020-06-12 10:33:37 +0900

    はい (#40)

--- config/init.go
+++ config/init.go
@@ -1,35 +1,28 @@
 package config
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"path/filepath"
 	"runtime"
-
-	"golang.org/x/xerrors"
 )
 
 func Initialize() error {
-	gopath := os.Getenv("GOPATH")
-	repoPath := filepath.Join(gopath, "src", "github.com", "xztaityozx", "go-cdx")
 
-	scriptPath := filepath.Join(repoPath, "script", func() string {
-		if runtime.GOOS == "windows" {
-			return "cdx.ps1"
-		} else {
-			return "cdx.sh"
-		}
-	}())
-
-	fp, err := os.Open(scriptPath)
-	if err != nil {
-		return xerrors.Errorf("cdx cannot found init script :%v", scriptPath)
-	}
-	defer fp.Close()
-	s := bufio.NewScanner(fp)
-	for s.Scan() {
-		fmt.Println(s.Text())
+	if runtime.GOOS == "windows" {
+		fmt.Println(`
+function cdx() {
+    begin{
+        [System.Collections.Generic.List[string]]$paths=@{}
+    }
+    process{
+        $paths.Add($_)
+    }
+    end{
+        $command = "$($paths | go-cdx $args)";
+        Invoke-Expression "$command"
+    }
+}`)
+	} else {
+		fmt.Println(`function() cdx() {command="$(go-cdx $@)"; eval "${command}"}`)
 	}
 
 	return nil
