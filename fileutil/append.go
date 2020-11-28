f8a62fc4af5d2ca9be1d445762b3cd8dd503e4f9
xztaityozx <taityonohibi@gmail.com>
2020-11-28 16:51:10 +0900

    delete deadcode

--- fileutil/append.go
+++ fileutil/append.go
@@ -13,11 +13,9 @@ func Append(path, text string) error {
 
 	defer fp.Close()
 
-  _, err = fp.WriteString(text)
+	_, err = fp.WriteString(text)
 	return err
 }
-
-var newline string
 
 func AppendLine(path, text string) error {
 	fp, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
@@ -27,6 +25,6 @@ func AppendLine(path, text string) error {
 
 	defer fp.Close()
 
-  _, err = fmt.Fprintln(fp, text)
-  return err
+	_, err = fmt.Fprintln(fp, text)
+	return err
 }
